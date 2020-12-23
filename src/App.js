f52fe7bbc43b7d7431b4abde37c2a7ad44ca31dc
Matthew <matt-hope@hotmail.com>
2020-12-22 20:15:28 -0500

    added react router import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import firebase from './firebase';
 import 'firebase/auth';
 import './App.css';
